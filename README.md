e0e8946ecf92e768ee4b90f8b139e6b68d3bc203
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-03-02 22:14:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sync EtuUTT -> RobertManager
+Outil de synchronisation de EtuUTT vers RobertManager
+
+## Installation
+```
+cloner le repository
+cp .env .example # Remplir le .env
+yarn
+yarn dev
+```
---
+++ main.js
@@ -0,0 +1,122 @@
+require('dotenv').config();
+const knex = require('knex');
+const _ = require('lodash');
+const chalk = require('chalk');
+
+const date = () => {
+  const currentdate = new Date();
+  return (
+    currentdate.getDate() +
+    '/' +
+    (currentdate.getMonth() + 1) +
+    '/' +
+    currentdate.getFullYear() +
+    ' @ ' +
+    currentdate.getHours() +
+    ':' +
+    currentdate.getMinutes() +
+    ':' +
+    currentdate.getSeconds()
+  );
+};
+
+const clientLog = (msg) => {
+  if (msg) console.log(chalk.blue(`[Client] [${date()}] ${msg}`));
+};
+
+const serverLog = (msg) => {
+  if (msg) console.log(chalk.red(`[Server] [${date()}] ${msg}`));
+};
+
+const neutralLog = (msg) => {
+  if (msg) console.log(chalk.yellow(`[${date()}] ${msg}`));
+};
+
+const nullFields = {
+  user_id: null,
+  email: null,
+  phone: null,
+  street: null,
+  postal_code: null,
+  locality: null,
+  country_id: null,
+  company_id: null,
+  note: null
+};
+
+const testConnection = async (instance) => {
+  try {
+    await instance.raw('select 1+1 as result');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+(async () => {
+  const server = knex({
+    client: 'mysql',
+    connection: {
+      host: process.env.SERVER_HOST,
+      user: process.env.SERVER_USER,
+      password: process.env.SERVER_PASSWORD,
+      database: process.env.SERVER_DATABASE
+    }
+  }).on('query', (query) => {
+    serverLog(query.sql);
+  });
+
+  const client = knex({
+    client: 'mysql',
+    connection: {
+      host: process.env.CLIENT_HOST,
+      user: process.env.CLIENT_USER,
+      password: process.env.CLIENT_PASSWORD,
+      database: process.env.CLIENT_DATABASE
+    }
+  }).on('query', (query) => {
+    clientLog(query.sql);
+  });
+
+  await testConnection(server);
+  await testConnection(client);
+
+  neutralLog(`Sync every ${process.env.SYNC_GAP_MINUTES} minutes`);
+
+  const sync = async () => {
+    const serverRows = await server('etu_users')
+      .select('firstName', 'lastName', 'login')
+      .whereRaw('bdeMembershipEnd > NOW()');
+
+    const clientRows = await client('persons')
+      .select('nickname AS login')
+      .whereNull('user_id');
+
+    const peopleToAdd = _.differenceBy(serverRows, clientRows, 'login');
+    const peopleToRemove = _.differenceBy(clientRows, serverRows, 'login');
+
+    if (peopleToAdd.length > 0) {
+      await client('persons').insert(
+        peopleToAdd.map((user) => ({
+          first_name: user.firstName,
+          last_name: user.lastName,
+          nickname: user.login,
+          ...nullFields
+        }))
+      );
+    }
+    if (peopleToRemove.length > 0) {
+      await client('persons')
+        .whereIn(
+          'nickname',
+          peopleToRemove.map((user) => user.login)
+        )
+        .del();
+    }
+
+    neutralLog(`${peopleToAdd.length} users added`);
+    neutralLog(`${peopleToRemove.length} users removed`);
+  };
+  sync();
+  setInterval(sync, process.env.SYNC_GAP_MINUTES * 60 * 1000);
+})();
